45550e15e4223fe81ced935c20159be23489fe13
Noh Hyeon Jin <hjforj@naver.com>
2022-04-28 15:07:41 +0900

    initial commit

---
+++ src/main/java/com/noh/Sibun_SpringBoot/SibunSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.noh.Sibun_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SibunSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SibunSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noh/Sibun_SpringBoot/SibunSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noh.Sibun_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SibunSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
